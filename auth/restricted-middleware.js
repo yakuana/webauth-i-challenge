b7637a4003c3cbdad31ea51dfb26c5331b8f31ff
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-09-16 16:38:02 -0400

    WIP: auth middleware created and complete.

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs'); 
+
+const db = require('../register/register-model.js'); 
+
+const validate = (req, res, next) => {
+    // get password and username from headers (not body)
+    let { username, password } = req.headers; 
+
+    // use data base (register-model) to check if username exists
+    db.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, username.password)) {
+                // password success 
+                next(); 
+            } else {
+                // password failure 
+                res.status(401).json({ 
+                    message: 'You cannot pass!' 
+                })
+            }
+        })
+        .catch(error => {
+            // username not in database 
+            res.status(500).json({
+                message: 'Invalid Credentials', 
+                error: error 
+            }); 
+        })
+}
+
+module.export = validate; 
