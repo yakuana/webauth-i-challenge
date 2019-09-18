e85ccde04a7e0e6428819d0504969c31ecbc5aaa
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-09-18 15:08:51 -0400

    WIP: Added neccessary dependecnies.

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,34 +1,51 @@
-const bcrypt = require('bcryptjs'); 
+//  第二天
 
-const db = require('../register/register-model.js'); 
+module.exports = (req, res, next) => {
+    // do we have information about the user in our session?
+    if (req.session && req.session.user) {
+        // successful
+        next();
+    } else {
+        //unsuccessful 
+        res.status(401).json({ 
+            message: 'You shall not pass!' 
+        });
+    }
+  };
+  
 
-const validate = (req, res, next) => {
-    // get password and username from headers (not body)
-    let { username, password } = req.headers; 
+// 第一天
+// const bcrypt = require('bcryptjs'); 
 
-    // use data base (register-model) to check if username exists
-    db.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                // password success 
-                next(); 
-            } else {
-                console.log("pass and user.pass", password, user.password)
-                // password failure 
-                res.status(401).json({ 
-                    message: 'You cannot pass!' 
-                })
-            }
-        })
-        .catch(error => {
-            // username not in database 
+// const db = require('../register/register-model.js'); 
 
-            res.status(500).json({
-                message: 'Invalid Credentials', 
-                error: error 
-            }); 
-        })
-}
+// const validate = (req, res, next) => {
+//     // get password and username from headers (not body)
+//     let { username, password } = req.headers; 
 
-module.exports = validate; 
+//     // use data base (register-model) to check if username exists
+//     db.findBy({ username })
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//                 // password success 
+//                 next(); 
+//             } else {
+//                 console.log("pass and user.pass", password, user.password)
+//                 // password failure 
+//                 res.status(401).json({ 
+//                     message: 'You cannot pass!' 
+//                 })
+//             }
+//         })
+//         .catch(error => {
+//             // username not in database 
+
+//             res.status(500).json({
+//                 message: 'Invalid Credentials', 
+//                 error: error 
+//             }); 
+//         })
+// }
+
+// module.exports = validate; 
